96888c4154c0e189cb8066c8f1305f6f07157c1c
GuoSiAo <553690204@qq.com>
2022-07-14 00:58:10 +0800

    pom.xml配置

---
+++ src/test/java/com/gsa/seckill2022/Seckill2022ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gsa.seckill2022;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Seckill2022ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
